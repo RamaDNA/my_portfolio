706646d1148a4f06ad2208895a8b1341887e77b4
nofian bismika ariyatma <rama99ever@gmail.com>
2023-09-16 06:17:35 +0700

    Update data and new Machine Learning

--- app.py
+++ app.py
@@ -15,11 +15,29 @@ def load_lottie_url(url):
         return None
     return r.json()
 
-#create predict function
+#create predict function Netflix stock
 def predict(x1,x2,x3):
     model = joblib.load('linear_regression_model.pkl')
     prediction = model.predict([[x1, x2, x3]])
     return prediction[0]
+
+#create predict function Sentiment App Gojek
+def predict_sentiment(text):
+    model = joblib.load('RandomForest.pkl')
+    tfidf = joblib.load('tfidf_vectorizer.pkl')
+    content = [text]
+    content_transformed = tfidf.transform(content)
+    prediction = model.predict(content_transformed.toarray())
+    return prediction[0]
+
+#create function validate Machine Learning input
+def validate_sentiment(text):
+    text_sentiment = text
+    if text_sentiment.strip():
+        prediction = predict_sentiment(text_sentiment)
+        st.write("The Prediction is",prediction)
+    else:
+        st.write("Please provide feedback to get a prediction.")
 
 # END FUNCTION ------------------------------------------------------------
 
@@ -40,7 +58,7 @@ if choice == 'Home':
     #welcome
     with st.container():
         st.header("welcome")
-        st.write("this is my streamlit website")
+        st.write("This is my Portfolio")
         st.title("Hello my name is Rama Dona Ariyatma")
         st.write("I'm Data Science, web progammer from Indonesia")
     #what i do
@@ -52,12 +70,19 @@ if choice == 'Home':
         with lottie_animation:
             st.header("what I do")
             st.write("""
-            - Create A model for machine learning using Python and mining data
-                - Create model linear regression
-                - Create model K-means
-                - Create model DecisionTree  
+            - Create A model for machine learning or Deep Learning using Python and mining data
+                - Cleaning data
+                - Scrapping data
+                - Create Model
+                - NLTK
+                - TensorFlow
+                - Keras
+                - Visualization
             - In Web progammer
                 - Using php and framework like laravel i know about OOP and MVC
+                - Bootstrap
+                - CSS
+                - Javascript
             - Mobile dev
                 - I try flutter
                 - Kotlin""")
@@ -82,13 +107,13 @@ if choice == 'Home':
             st.write("rama99ever@gmail.com")
         with whatsapp:
             st.write("my whatsapp")
-            st.write("081-1111-0222")
+            st.write("+62-813-817-37058")
         with instragram:
             st.write("my Instagram")
             st.markdown("[My instagram](https:www.instagram.com)")
         with facebook:
             st.write('my facebook')
-            st.markdown("[my facebook] (https:www.facebook.com)")
+            st.markdown("[my facebook](https:www.facebook.com)")
 
 elif choice == 'My project':
     st.header("This is my project")
@@ -106,4 +131,14 @@ elif choice == 'My project':
             st.write("You can predict the end price just input open price,high price, low price and get predicted")
     with st.container():
         st.write("---")
-        st.header(" I Will update Soon to upload my next project")
+        input_form,explanation = st.columns(2)
+        with input_form:
+            st.header("Prediction Sentiment App Gojek")
+            text_sentiment = st.text_area('Enter your Feedback App Gojek')
+            validate_sentiment(text_sentiment)
+        with explanation:
+            st.header("Explain Machine Learning")
+            st.write("We can prediction what's meaning feedback is Positif or Negatif just insert ur comment")
+    with st.container():
+        st.write("---")
+        st.header("I Will update Soon to upload my next project")
